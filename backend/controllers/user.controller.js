b690a9129c533f5068a7c3dd9fc1a72b846b7148
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-12-04 22:39:30 +0530

    User register and login

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,51 @@
+const userModel=require('../models/user.model')
+const userService=require('../services/user.service')
+const {validationResult}=require('express-validator')
+
+module.exports.registerUser=async (req,res,next)=>{
+   const errors=validationResult(req);
+
+   if(!errors.isEmpty()){
+       return res.status(400).json({errors:errors.array()});
+   }
+   console.log(req.body)
+   const {fullName,email,password}=req.body;
+   const hashedPassword=await userModel.hashPassword(password);
+
+   //creating user
+   const user=await userService.createUser({
+    firstName:fullName.firstName,
+    lastName:fullName.lastName,
+    email,
+    password:hashedPassword
+   })
+
+   const token=user.generateAuthToken();
+   res.status(200).json({token,user})
+}
+
+module.exports.loginUser=async (req,res,next)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+    const {email,password}=req.body;
+    const user=await userModel.findOne({email}).select('+password');
+
+    //if user not present
+    if(!user){
+        return res.status(401).json({message:'Invalid Email or Password'})
+    }
+   // check if password matches
+    const isMatch=await user.comparePassword(password);
+
+    // if password does not match
+    if(!isMatch){
+        return res.status(401).json({message:'Invalid Email or Password'})
+    }
+
+    //generating token
+    const token=user.generateAuthToken();
+    res.status(200).json({token,user});
+}
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -12,7 +12,8 @@ const userSchema=new mongoose.Schema({
         lastName:{
             type:String,
             minlength:[3,'Last name must be atleast 3 characters long']
-        },
+        }
+    },
         email:{
             type:String,
             required:true,
@@ -27,7 +28,7 @@ const userSchema=new mongoose.Schema({
         socketId:{
             type:String
         }
-    }
+    
 })
 
 userSchema.methods.generateAuthToken=function(){
