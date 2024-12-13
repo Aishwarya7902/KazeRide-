fac2558c2582a31f3166e5060b76e00179d315b9
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-12-13 22:53:50 +0530

    Integrated signup route for user

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,18 @@ import UserLogin from './page/UserLogin'
 import UserSignup from './page/UserSignup'
 import CaptainSignup from './page/CaptainSignup'
 import CaptainLogin from './page/CaptainLogin'
+import Start from './page/Start'
 
 const App = () => {
   return (
    <div>
      <Routes>
-         <Route path='/' element={<Home/>}/>
+         <Route path='/' element={<Start/>}/>
          <Route path='/login' element={<UserLogin/>}/>
          <Route path='/signup' element={<UserSignup/>}/>
          <Route path='/captain-signup' element={<CaptainSignup/>}/>
          <Route path='/captain-login' element={<CaptainLogin/>}/>
+         <Route path='/home' element={<Home/>}/>
      </Routes>
    </div>
   )
