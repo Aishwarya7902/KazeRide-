8bc74e8cb312bfd9e91b6e7499e960a87fee530d
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-12-14 13:15:58 +0530

    Integrated userLogin endpoint with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from './page/Home'
 import UserLogin from './page/UserLogin'
 import UserSignup from './page/UserSignup'
 import CaptainSignup from './page/CaptainSignup'
 import CaptainLogin from './page/CaptainLogin'
 import Start from './page/Start'
+import UserProtectWrapper from './page/UserProtectWrapper'
+import UserLogout from './page/UserLogout'
 
 const App = () => {
   return (
-   <div>
-     <Routes>
-         <Route path='/' element={<Start/>}/>
-         <Route path='/login' element={<UserLogin/>}/>
-         <Route path='/signup' element={<UserSignup/>}/>
-         <Route path='/captain-signup' element={<CaptainSignup/>}/>
-         <Route path='/captain-login' element={<CaptainLogin/>}/>
-         <Route path='/home' element={<Home/>}/>
-     </Routes>
-   </div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+        <Route path='/captain-login' element={<CaptainLogin />} />
+        <Route path='/home' element={
+          <UserProtectWrapper>
+            <Home />
+          </UserProtectWrapper>
+        } />
+        <Route path='/user/logout' element={
+          <UserProtectWrapper>
+            <UserLogout></UserLogout>
+          </UserProtectWrapper>
+        } />
+      </Routes>
+    </div>
   )
 }
 
--- frontend/src/page/UserLogin.jsx
+++ frontend/src/page/UserLogin.jsx
@@ -1,19 +1,41 @@
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import React, { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { userDataContext } from '../context/userContext';
+import axios from 'axios';
 const UserLogin = () => {
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
-    const [userData,setUserData]=useState({});
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [userData, setUserData] = useState({});
+    const navigate = useNavigate();
+    const [user, setUser] = useContext(userDataContext);
 
-    const submitHandler=(e)=>{
+    const submitHandler = async (e) => {
         e.preventDefault();
-        setUserData({
-            email,
-            password
-        })
-        setEmail('');
-        setPassword('');
-        console.log(userData)
+        if (!email || !password) {
+            alert("All fields are required");
+            return;
+        }
+        try {
+            const newUser = {
+                email,
+                password
+            }
+
+            const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/login`, newUser);
+            if (response.status === 200) {
+                const data = response.data;
+                setUser(data.user);
+                localStorage.setItem('token',data.token);
+                navigate('/home');
+            }
+            setEmail('');
+            setPassword('');
+        } catch (error) {
+            console.error(error);
+            alert('Registration failed. Please try again.');
+        }
+
+
 
     }
 
@@ -22,7 +44,7 @@ const UserLogin = () => {
         <div className='p-7 h-screen flex flex-col justify-between'>
             <div>
                 <img className='w-16 mb-10' src='https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png'></img>
-                <form onSubmit={(e)=>{
+                <form onSubmit={(e) => {
                     submitHandler(e)
                 }}>
                     <h3 className='text-lg font-medium mb-2'>What's your Email?</h3>
@@ -33,7 +55,7 @@ const UserLogin = () => {
                         placeholder='email@example.com'
                         className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
                         value={email}
-                        onChange={(e)=>{
+                        onChange={(e) => {
                             setEmail(e.target.value)
                         }}
                     />
@@ -46,13 +68,13 @@ const UserLogin = () => {
                         placeholder='password'
                         className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
                         value={password}
-                        onChange={(e)=>{
+                        onChange={(e) => {
                             setPassword(e.target.value);
                         }}
                     />
                     <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2  w-full text-lg placeholder:text-base">Login
                     </button>
-                    
+
                 </form>
                 <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p>
             </div>
