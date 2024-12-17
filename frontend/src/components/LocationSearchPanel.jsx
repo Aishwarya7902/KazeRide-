aa649044cc30d2cc95544716ace9fa3e160e6189
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-12-17 13:45:05 +0530

    Creating Location search panel UI

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const LocationSearchPanel = () => {
   return (
-    <div>LocationSearchPanel</div>
+    <div>
+        <div className='flex gap-4 items-center justify-center my-4'>
+            <h2 className='bg-[#eee] h-8 w-12  rounded-full flex items-center justify-center'><i className="ri-map-pin-fill "></i></h2>
+            <h4 className='font-medium'>24B, Near Kapoor's cafe, DAV Coding School,Patna</h4>  
+        </div>
+
+        <div className='flex gap-4 items-center justify-center my-4'>
+            <h2 className='bg-[#eee] h-8 w-12  rounded-full flex items-center justify-center'><i className="ri-map-pin-fill "></i></h2>
+            <h4 className='font-medium'>24B, Near Kapoor's cafe, DAV Coding School,Patna</h4>  
+        </div>
+
+        <div className='flex gap-4 items-center justify-center my-4'>
+            <h2 className='bg-[#eee] h-8 w-12  rounded-full flex items-center justify-center'><i className="ri-map-pin-fill "></i></h2>
+            <h4 className='font-medium'>24B, Near Kapoor's cafe, DAV Coding School,Patna</h4>  
+        </div>
+
+        <div className='flex gap-4 items-center justify-center my-4'>
+            <h2 className='bg-[#eee] h-8 w-12  rounded-full flex items-center justify-center'><i className="ri-map-pin-fill "></i></h2>
+            <h4 className='font-medium'>24B, Near Kapoor's cafe, DAV Coding School,Patna</h4>  
+        </div>
+
+    </div>
   )
 }
 
