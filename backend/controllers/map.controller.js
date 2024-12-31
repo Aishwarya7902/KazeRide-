0a032fd1010d3504d4cdbae7398259e4fcdf5c57
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-12-31 23:35:37 +0530

    small correction in map controller

--- backend/controllers/map.controller.js
+++ backend/controllers/map.controller.js
@@ -1,19 +1,20 @@
-const mapService=require('../services/maps.service')
-const {validationResult}=require('express-validator')
+const mapService = require('../services/maps.service')
+const { validationResult } = require('express-validator')
 
 
-module.exports.getCoordinates=async (req,res,next)=>{
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        res.status(400).json({errors:errors.array()})
+module.exports.getCoordinates = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() })
     }
 
-    const {address}=req.query;
-    try{
-        const coordinates=mapService.getAddressCoordinate(address);
-        res.status(200).json(coordinates)
+    const { address } = req.query;
+    try {
+        const coordinates = await mapService.getAddressCoordinate(address);
+        res.status(200).json(coordinates);
+
     }
-    catch(error){
-        res.status(404).json({message:'Coordinates not found'})
+    catch (error) {
+        res.status(404).json({ message: 'Coordinates not found' })
     }
 }
