a6f12f62d1c3020a718b2b8ffd424a3bf3445c92
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-01-01 20:59:39 +0530

    distanceTime endpoint added

--- backend/services/maps.service.js
+++ backend/services/maps.service.js
@@ -9,7 +9,7 @@ module.exports.getAddressCoordinate = async (address) => {
         const response = await axios.get(url);
         console.log('API Response:', response.data); // Log full API response
         if (response.data.status === 'OK') {
-            const location = response.data.results[ 0 ].geometry.location;
+            const location = response.data.results[0].geometry.location;
             return {
                 ltd: location.lat,
                 lng: location.lng
@@ -20,5 +20,36 @@ module.exports.getAddressCoordinate = async (address) => {
     } catch (error) {
         console.error(error);
         throw error;
+    }
+}
+
+module.exports.getDistanceTime = async (origin, destination) => {
+    if (!origin || !destination) {
+        throw new Error('Origin and destination are required')
+    }
+    const apiKey = process.env.GOOGLE_MAPS_API;
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${apiKey}`;
+
+
+    try {
+
+        const response = await axios.get(url)
+
+        if (response.data.status === 'OK') {
+            if (response.data.rows[0].elements[0].status === 'ZERO_RESULTS') {
+                throw new Error('No routes found')
+            }
+            
+            return response.data.rows[0].elements[0]
+        }
+
+        else {
+            throw new Error('Unable to fetch distance and time')
+        }
+
+    }
+    catch (err) {
+        console.error(err)
+        throw err;
     }
 }
